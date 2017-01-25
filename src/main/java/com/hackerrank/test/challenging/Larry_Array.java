c1e013ae49bc84cce50794854142f6652ba99078
jackalhan <jackalhan@gmail.com>
2017-01-25 01:43:16 -0600

    new files

---
+++ src/main/java/com/hackerrank/test/challenging/Larry_Array.java
@@ -0,0 +1,49 @@
+package com.hackerrank.test.challenging;
+
+import java.util.Scanner;
+
+/**
+ * Created by jackalhan on 1/20/17.
+ */
+public class Larry_Array {
+   /* public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nTestCase = scanner.nextInt();
+        for (int i = 0; i < nTestCase; i++) {
+            int arrSize = scanner.nextInt();
+            int[] array = new int[arrSize];
+            for (int ia = 0; ia < arrSize; ia++) {
+                array[ia] = scanner.nextInt();
+            }
+
+            boolean isSorted = false;
+            for (int k = 0; k <= arrSize - 3; k++) {
+                if ()
+                while ()for (int j = 0; j < 3; j++) {
+                    int tmp = 0;
+                    if (array[k] < array[k+1])
+                    {
+                        tmp = array[k];
+                        array[k] = array[k+1];
+                        array[k+1] = array[k+2];
+                        array[k+2] = tmp;
+
+                    }
+                    else if (array[k] < array[k+1])
+                    {
+                        tmp = array[k];
+                        array[k] = array[k+1];
+                        array[k+1] = array[k+2];
+                        array[k+2] = tmp;
+
+                    }
+                    else
+                    {
+                        isSorted = true;
+                    }
+                }
+
+            }
+        }
+    }*/
+}
