922b17e07ad152df236abd4af216a4496905d728
jackalhan <jackalhan@gmail.com>
2017-01-26 01:19:35 -0600

    committing

---
+++ src/main/java/com/hackerrank/test/tutorial/demo.java
@@ -0,0 +1,18 @@
+package com.hackerrank.test.tutorial;
+
+/**
+ * Created by jackalhan on 1/26/17.
+ */
+public class demo {
+    public static void main(String[] args) {
+        int counter = 0;
+        counter+=2;
+        System.out.println(counter);
+        counter=+2;
+        System.out.println(counter);
+        counter++;
+        System.out.println(counter);
+        counter+=5;
+        System.out.println(counter);
+    }
+}
