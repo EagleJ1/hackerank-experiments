ef28df98fe1326117fd710803b3ae2dd9b26dda1
jackalhan <jackalhan@gmail.com>
2017-02-02 16:44:29 -0600

    committing.

---
+++ src/main/java/com/hackerrank/test/challenging/Lucky_Numbers.java
@@ -0,0 +1,38 @@
+package com.hackerrank.test.challenging;
+
+import java.util.Scanner;
+
+/**
+ * Created by txcakaloglu on 2/1/17.
+ */
+public class Lucky_Numbers {
+
+    // 4 & 7
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        String result = "";
+        for (int a0 = 0; a0 < q; a0++) {
+            long n = in.nextLong();
+            int remainder11 = (int) (n % 11);
+            int remainder4 = (int) (n % 4);
+            int remainder7 = (int) (n % 7);
+            if (remainder11 == 0 || remainder4 == 0 || remainder7 == 0) {
+                result = "Yes";
+            } else {
+                if (remainder11 % 4 == 0 || remainder11 % 7 == 0) {
+                    result = "Yes";
+                } else {
+                    result = "No";
+                }
+
+            }
+
+            System.out.println(result);
+
+        }
+
+
+    }
+}
