4bd7539f0041ad763981cb99b621bc9d3f078b44
jackalhan <jackalhan@gmail.com>
2017-02-08 16:00:15 -0600

    committing.

--- src/main/java/com/hackerrank/test/challenging/Subarray.java
+++ src/main/java/com/hackerrank/test/challenging/Subarray.java
@@ -12,8 +12,21 @@ public class Subarray {
         int arrSize = scanner.nextInt();
         int[] arr = new int[arrSize];
         while (arrSize-- > 0) {
-            arr[arr.length - arrSize] = scanner.nextInt();
+            arr[arr.length - 1- arrSize] = scanner.nextInt();
         }
+
+        int counter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int subTotal = 0;
+            for (int j = i; j < arr.length; j++) {
+                subTotal = subTotal + arr[j];
+                if (subTotal < 0) {
+                    counter++;
+                }
+            }
+        }
+
+        System.out.println(counter);
         scanner.close();
     }
 }
