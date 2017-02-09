d41abe050acccc8aed8c39119478b2798eb05f68
jackalhan <jackalhan@gmail.com>
2017-02-09 01:15:43 -0600

    committing

---
+++ src/main/java/com/hackerrank/test/patterns/AnimalPlay.java
@@ -0,0 +1,25 @@
+package com.hackerrank.test.patterns;
+
+/**
+ * Created by jackalhan on 2/9/17.
+ */
+public class AnimalPlay {
+    public static void main(String[] args) {
+
+        /* ***********************/
+        /* STRATEGY PATTERNS    */
+        /* ***********************/
+        // We are going to try what we have done for POLYMORPHICALLY in Animal, Dog, Bird classes and FLYS interface
+        Animal sparky = new Dog();
+        Animal tweety = new Bird();
+
+        System.out.println("Dog : " + sparky.tryToFly());
+        System.out.println("Bird : " + tweety.tryToFly());
+
+        //Let us DYNAMICALLY set FLYING_ABILITY or behaviours of the animals.
+        sparky.setFlyingAbility(new ItFlys());
+        System.out.println("After changed flying ability of Dog : " + sparky.tryToFly());
+        tweety.setFlyingAbility(new ItFlysSuperFast());
+        System.out.println("After changed flying ability of Bird : " + tweety.tryToFly());
+    }
+}
---
+++ src/main/java/com/hackerrank/test/patterns/Flys.java
@@ -0,0 +1,62 @@
+package com.hackerrank.test.patterns;
+
+/**
+ * Created by jackalhan on 2/9/17.
+ */
+/* ***********************/
+/* STRATEGY PATTERNS    */
+/* ***********************/
+// ADVANTAGES
+// -----------
+// * to remove code duplication and manupalations
+// * reduces long lists of conditionals (many many type of conditions, SP it helps)
+// * Keeps class changes from forcing other class changes
+// * Can hide complicated / secret code from the user
+// DISADVANTAGE
+// -----------
+// * Increased Number of Objects / Classes
+// ==========================================================================
+
+/* We are going to use this interface inside of animal as an instance
+*  variable and then we are going to dynamically change that instance varialbe to
+*  be either of type class ITFLYS or CANTFLY.
+*
+*  Why is it a great idea?
+*  Because inteface is implemented by many other different subclasses
+*  and what this is going to allow us to do is create many different types of
+*  flying WITHOUT AFFECTING ANIMAL or ANY OF THESE SUBCLASSES.
+*  And classes that implement this new FLY interface are going to allow those
+*  classes to use that code while also eliminating code duplication which means that
+*  we are encapsulating the concept for the behaviour that varies and that behaviour concept
+*  is capability to fly.
+*
+* */
+public interface Flys {
+    String fly();
+
+}
+
+class ItFlys implements Flys {
+
+    @Override
+    public String fly() {
+        return "Flying High";
+    }
+}
+
+class CantFly implements Flys {
+
+    @Override
+    public String fly() {
+        return "I can't fly";
+    }
+}
+
+// I can even add more behaviour to this interface such as;
+class ItFlysSuperFast implements Flys {
+
+    @Override
+    public String fly() {
+        return "I am flying SUPER fast";
+    }
+}
